34cb7d2951eeb2dc604b7c11c8d30131ad9c1b6e
Raul Menezes <raul@rmenezes.me>
2026-04-09 23:39:29 +0100

    Bump swift-syntax to 603.0.0

--- swift-macro/Package.swift
+++ swift-macro/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "603.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
     ],
     targets: [
